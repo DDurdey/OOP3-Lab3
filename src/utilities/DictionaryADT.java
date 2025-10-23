589688ef8bf536744c55e6d12c63aa95eae6c2bd
DDurdey <daniel.durdey@edu.sait.ca>
2025-10-23 10:35:00 -0600

    Initial commit

---
+++ src/utilities/DictionaryADT.java
@@ -0,0 +1,21 @@
+package utilities;
+
+/**
+ * DictionaryADT.java
+ *
+ * @author your name
+ * @version major.minor revision number starting at 1.0	
+ * 
+ * <p>
+ * The <code>DictionaryADT</code> interface is designed to be used as a basis for 
+ * the Dictionary data structure that will be developed in the CPRG304 F2025 class
+ * at SAIT. 
+ * </p>
+ * 
+ * @param <K,V> The key-value pairs this list holds.
+ */
+
+public interface DictionaryADT<K,V>
+{
+
+}
